2c37de48604c1e6f764a5dcf6cb183eb094f1541
BillDueber <bill@dueber.com>
2010-09-22 09:28:39 -0400

    Initial commit to marc2solr.

---
+++ spec/marc2solr_spec.rb
@@ -0,0 +1,7 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "Marc2solr" do
+  it "fails" do
+    fail "hey buddy, you should probably rename this file and start specing for real"
+  end
+end
